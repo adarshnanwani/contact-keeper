2f25ccf29a86e3988c9513d9cfbd88f64456a124
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-04-19 20:19:07 +0530

    Add auth context and hook it up with App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,31 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/'>
-                <Home />
-              </Route>
-              <Route exact path='/about'>
-                <About />
-              </Route>
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/'>
+                  <Home />
+                </Route>
+                <Route exact path='/about'>
+                  <About />
+                </Route>
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
