3be2113d599de7c4c871ba0f0e524122908ee077
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-04-19 19:46:25 +0530

    Add contact filter.

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,14 +2,22 @@ import React, { useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
 
-export const Contacts = (props) => {
+export const Contacts = () => {
   const contactContext = useContext(ContactContext);
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
   return (
     <>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered === null
+        ? contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </>
   );
 };
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -33,6 +33,19 @@ const contactReducer = (state, action) => {
         ...state,
         contacts: contactsCopy,
       };
+    case FILTER_CONTACTS:
+      return {
+        ...state,
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, 'gi');
+          return contact.name.match(regex) || contact.email.match(regex);
+        }),
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null,
+      };
     case SET_CURRENT:
       return {
         ...state,
