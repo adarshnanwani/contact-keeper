494f2555bd638f8f880ead72eb9d890f033ba6d2
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-04-24 23:22:16 +0530

    Update contact API integration.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -58,11 +58,40 @@ const ContactState = (props) => {
   };
 
   // Delete Contact
-  const deleteContact = (id) => {
-    dispatch({
-      type: DELETE_CONTACT,
-      payload: id,
-    });
+  const deleteContact = async (id) => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id,
+      });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   // Clear Contacts
@@ -84,14 +113,6 @@ const ContactState = (props) => {
   const clearCurrent = () => {
     dispatch({
       type: SET_CURRENT,
-    });
-  };
-
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({
-      type: UPDATE_CONTACT,
-      payload: contact,
     });
   };
 
